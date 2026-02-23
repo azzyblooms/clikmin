e1f78bacf2b768c61a430941fb2140be48059d8a
azzyblooms <azzyblooms@gmail.com>
2026-02-23 19:09:23 +0000

    forgot to commit ts. favicon + busywork

--- clikmin.js
+++ clikmin.js
@@ -1,13 +1,19 @@
 const clickSound = new Audio('audio/meow.mp3');
 const clickableMin = document.getElementById('clickablemin');
 const number = document.getElementById('coolnumber');
+const pageTitle = document.getElementById('pagetitle');
 let pokos = 0;
-
 
 clickableMin.addEventListener('click', () => {
     clickSound.cloneNode(true).play();
     pokos++;
     number.textContent = pokos;
+    if(pokos = 1) {
+        pageTitle.textContent = "clikmin: " + pokos + " poko"
+    } 
+    if(pokos != 1) {
+        pageTitle.textContent = "clikmin: " + pokos + " pokos"
+    }
     number.style.transform = "scale(1.2)";
     number.style.transition = "ease 50ms"
     setTimeout(() => {
